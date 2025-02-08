ccaf2c3313275cf5a07beef0228eae67bfa4b48f
Ysanchez1108 <ysanchez1108@gmail.com>
2025-02-08 14:43:59 -0500

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def sort_list(input_list):
     for i in range(len(input_list)):
         for j in range(len(input_list) - 1):
             if input_list[j] > input_list[j + 1]:
-                input_list[j], input_list[j] = input_list[j + 1], input_list[j]
+                input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
     print(input_list)
     return input_list
+
+lista =[3,4,6,8,1,9]
+sort_list(lista)
